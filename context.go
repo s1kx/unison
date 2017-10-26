5a9fd8a5f5325c75918100ce27c08518265775d7
Anders Øen Fylling <7851860+andersfylling@users.noreply.github.com>
2017-10-26 22:00:55 +0200

    added interupt signal

--- context.go
+++ context.go
@@ -1,15 +1,31 @@
 package unison
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // Context is a type that is passed to every handler
 // in a bot application.
 // It can be used to refer back to main components.
 type Context struct {
-	Bot     *Bot
-	Discord *discordgo.Session
+	Bot                *Bot
+	Discord            *discordgo.Session
+	SystemInteruptChan chan struct{}
 }
 
 func NewContext(bot *Bot, ds *discordgo.Session) *Context {
-	return &Context{Bot: bot, Discord: ds}
+	is := make(chan struct{})
+
+	// shutdown signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		close(is)
+	}()
+
+	return &Context{Bot: bot, Discord: ds, SystemInteruptChan: is}
 }
