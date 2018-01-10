52c1f92cc3e48acf81d2f108754f0ffba0c01614
Anders Øen Fylling <7851860+andersfylling@users.noreply.github.com>
2018-01-10 19:23:21 +0100

    Implemented usage of environment prefix

--- unison.go
+++ unison.go
@@ -22,6 +22,11 @@ func Run(settings *Config) error {
 	// Configure logger format.
 	logrus.SetFormatter(&logFormatter)
 
+	// add a underscore suffix to environment prefix
+	if settings.EnvironmentPrefix != "" {
+		settings.EnvironmentPrefix = settings.EnvironmentPrefix + "_"
+	}
+
 	// TODO: Validate commands
 
 	// three steps are done before setting up a connection.
@@ -35,11 +40,11 @@ func Run(settings *Config) error {
 	token := settings.Token
 	// if it was not specified in the Settings struct, check the environment variable
 	if token == "" {
-		token = os.Getenv(constant.EnvUnisonDiscordToken)
+		token = os.Getenv(settings.EnvironmentPrefix + constant.EnvUnisonDiscordToken)
 
 		// if the env var was empty as well, crash the bot as this is required.
 		if token == "" {
-			return errors.New("Missing env var " + constant.EnvUnisonDiscordToken + ". This is required. Specify in either Settings struct or env var.")
+			return errors.New("Missing env var " + settings.EnvironmentPrefix + constant.EnvUnisonDiscordToken + ". This is required. Specify in either Settings struct or env var.")
 		}
 
 		logrus.Info("Using bot token from environment variable.")
@@ -57,7 +62,7 @@ func Run(settings *Config) error {
 
 	// 2. Set a prefered way of triggering commands
 	//
-	envPrefix := os.Getenv(constant.EnvUnisonCommandPrefix)
+	envPrefix := os.Getenv(settings.EnvironmentPrefix + constant.EnvUnisonCommandPrefix)
 	if envPrefix != "" {
 		settings.CommandPrefix = append(settings.CommandPrefix, envPrefix)
 	}
@@ -76,7 +81,7 @@ func Run(settings *Config) error {
 	// check if valid state
 	if uState == state.MissingState {
 		// chjeck environment variable
-		uStateStr := os.Getenv(constant.EnvUnisonState)
+		uStateStr := os.Getenv(settings.EnvironmentPrefix + constant.EnvUnisonState)
 
 		if uStateStr == "" {
 			uState = state.Normal // uint8(1)
