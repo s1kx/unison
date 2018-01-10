25a79c2ed04dd1585214e20987430cc3181b6ef8
Anders Øen Fylling <7851860+andersfylling@users.noreply.github.com>
2018-01-10 19:22:44 +0100

    Environment variable prefix added to Config

--- Config.go
+++ Config.go
@@ -5,9 +5,10 @@ import "github.com/andersfylling/unison/state"
 // Config contains the definition of bot behavior.
 // It is used while creating/setting up the actual bot.
 type Config struct {
-	Token         string
-	CommandPrefix []string
-	BotState      state.Type
+	Token             string
+	CommandPrefix     []string
+	BotState          state.Type
+	EnvironmentPrefix string // Put an environment prefix on all environment variables
 
 	// DisableMentionTrigger when true, bot commands won't execute when using mention as prefix
 	DisableMentionTrigger bool
