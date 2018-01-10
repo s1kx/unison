d989682020655c9b59c57c5880539ff4aaba800a
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-10 20:59:06 +0100

    Added fields for option struct

--- AuditLog.go
+++ AuditLog.go
@@ -7,19 +7,30 @@ import (
 )
 
 type AuditLogChange struct {
+	// will this even work? TODO, NOTE
 	NewValue interface{} `json:"new_value"`
 	OldValue interface{} `json:"old_value"`
 	Key      string      `json:"key"`
 }
 
+type AuditLogOption struct {
+	DeleteMemberDays string `json:"delete_member_days"`
+	MembersRemoved   string `json:"members_removed"`
+	ChannelID        string `json:"channel_id"`
+	Count            string `json:"count"`
+	ID               string `json:"id"`
+	Type             string `json:"type"`
+	RoleName         string `json:"role_name"`
+}
+
 type AuditLogEntry struct {
-	TargetID   string `json:"target_id"`
-	UserID     string `json:"user_id"`
-	ID         string `json:"id"`
-	ActionType uint64 `json:"action_type"`
-	//Changes    []*AuditLogChange `json:"changes"`
-	//Options []*
-	Reason string `json:"reason"`
+	TargetID   string            `json:"target_id"`
+	UserID     string            `json:"user_id"`
+	ID         string            `json:"id"`
+	ActionType uint64            `json:"action_type"`
+	Changes    []*AuditLogChange `json:"changes"`
+	Options    []*AuditLogOption `json:"options"`
+	Reason     string            `json:"reason"`
 }
 
 type AuditLog struct {
@@ -28,9 +39,57 @@ type AuditLog struct {
 	AuditLogEntries []*AuditLogEntry     `json:"audit_log_entries"`
 }
 
+// AuditLogParams set params used in endpoint request
+type AuditLogParams struct {
+	UserID     string `urlparam:"user_id,omitempty"`
+	ActionType int    `urlparam:"action_type,omitempty"`
+	Before     string `urlparam:"before,omitempty"`
+	Limit      int    `urlparam:"limit,omitempty"`
+}
+
+//
+// func convertAuditLogParamsToStr(params *AuditLogParams) string {
+// 	var getParams string
+//
+// 	v := reflect.ValueOf(*params)
+// 	t := reflect.TypeOf(*params)
+// 	// Iterate over all available fields and read the tag value
+// 	for i := 0; i < t.Elem().NumField(); i++ {
+// 		// Get the field, returns https://golang.org/pkg/reflect/#StructField
+// 		field := t.Field(i)
+//
+// 		// Get the field tag value
+// 		tag := field.Tag.Get("urlparam")
+//
+// 		// check if it's omitempty
+// 		tags := strings.Split(tag, ",")
+// 		if len(tags) > 1 {
+// 			var skip bool
+// 			for _, tagDetail := range tags {
+// 				if tagDetail == "omitempty" && reflect.DeepEqual(field, reflect.Zero(field.Type).Interface()) {
+// 					skip = true
+// 				}
+// 			}
+// 			if skip {
+// 				continue
+// 			}
+// 		}
+//
+// 		getParams += "&" + tags[0] + "=" + v.Field(i).Interface().(string)
+// 	}
+//
+// 	urlParams := ""
+// 	if getParams != "" {
+// 		urlParams = "?" + getParams[1:len(getParams)]
+// 	}
+//
+// 	return urlParams
+// }
+
 // GetAuditLogs Get the last 50 audit logs for the given guild
-func GetAuditLogs(ctx *Context, guildID string) (*AuditLog, error) {
-	byteArr, err := ctx.Discord.Request("GET", discordgo.EndpointGuilds+guildID+"/audit-logs", nil)
+func GetAuditLogs(ctx *Context, guildID string, params *AuditLogParams) (*AuditLog, error) {
+	urlParams := "" //convertAuditLogParamsToStr(params)
+	byteArr, err := ctx.Discord.Request("GET", discordgo.EndpointGuilds+guildID+"/audit-logs"+urlParams, nil)
 
 	auditLog := &AuditLog{}
 	err = json.Unmarshal(byteArr, &auditLog)
---
+++ auditLog_test.go
@@ -0,0 +1,12 @@
+package unison
+
+// import "testing"
+//
+// func TestConvertAuditLogParamsToStr(t *testing.T) {
+// 	params := &AuditLogParams{}
+// 	res := convertAuditLogParamsToStr(params)
+//
+// 	if res != "" {
+// 		t.Errorf("Empty AuditLogParams struct did not create an empty string. Got %s, wants \"\"", res)
+// 	}
+// }
