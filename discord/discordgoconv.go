b5227d64645f129db47458cb76164c04ee3e3541
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-13 20:52:21 +0100

    updated converters

--- discord/discordgoconv.go
+++ discord/discordgoconv.go
@@ -1,8 +1,15 @@
 package discord
 
-import "strconv"
+import (
+	"strconv"
+	"time"
 
-func discordIDStringToUint64(id string) uint64 {
+	"gopkg.in/bwmarrin/Discordgo.v0"
+)
+
+const discordgoTimestampLayout string = "2016-08-06T17:20:33.803-0400"
+
+func discordgoIDStringToUint64(id string) uint64 {
 	if id == "" {
 		return 0
 	}
@@ -17,4 +24,27 @@ func discordIDStringToUint64(id string) uint64 {
 
 func uint64ToString(id uint64) string {
 	return strconv.FormatUint(id, 10)
+}
+
+func discordgoTimestampToTime(ts string) time.Time {
+	timestamp, err := time.Parse(discordgoTimestampLayout, ts)
+	if err != nil {
+		panic(err)
+		//return time.Now() // this is so bad..
+	}
+
+	return timestamp
+}
+
+func discordgoUserArrayTODiscordUserArray(users []*discordgo.User) []*User {
+	discordUsers := make([]*User, 0, len(users))
+	for i, user := range users {
+		discordUsers[i] = NewUserFromDiscordgo(user)
+	}
+
+	return discordUsers
+}
+
+func discordgoCopyTodiscordStruct(discordgoStruct interface{}, discordStruct interface{}) {
+	// TODO use reflection to copy over values with similar type and json tag.
 }
