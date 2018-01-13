d3c3bfc30d4038409f301bd188dbee6253ac98d4
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-13 20:58:10 +0100

    added discordgo conversion for Attachment

--- discord/attachment.go
+++ discord/attachment.go
@@ -1,5 +1,9 @@
 package discord
 
+import (
+	"gopkg.in/bwmarrin/Discordgo.v0"
+)
+
 type Attachment struct {
 	ID       uint64 `json:"id"`
 	Filename string `json:"filename"`
@@ -8,4 +12,16 @@ type Attachment struct {
 	ProxyURL string `json:"proxy_url"`
 	Height   uint   `json:"height"`
 	Width    uint   `json:"width"`
+}
+
+func NewAttachmentFromDiscordgo(a *discordgo.MessageAttachment) *Attachment {
+	return &Attachment{
+		ID:       discordgoIDStringToUint64(a.ID),
+		Filename: a.Filename,
+		Size:     uint(a.Size),
+		URL:      a.URL,
+		ProxyURL: a.ProxyURL,
+		Height:   uint(a.Height),
+		Width:    uint(a.Width),
+	}
 }
