037d234848350c592582e25d7dc8c6ce74a52dcd
Patrick Glandien <patrick@synix.io>
2018-01-15 02:48:13 -0700

    Merge CommandPermission, probably delete soon

--- command.go
+++ command.go
@@ -165,3 +165,30 @@ func (cmd *Command) invoke(ctx *Context, msg *discordgo.Message, request string)
 		logrus.Errorf("Command [%s]: %s", cmd.Name, err)
 	}
 }
+
+// ########################
+// Deprecated
+// ########################
+
+// CommandPermission Designed to handle X guilds per bot instance
+// type CommandPermission struct {
+// 	// Used for specific User banning on a per command basis
+// 	BannedUserIDs []string // *discordgo.User.ID
+//
+// 	// Users that has been given access to a command even though they do not have the required role.
+// 	AllowedUserIDs []string // *discordgo.User.ID
+//
+// 	// What roles can run this command
+// 	AllowedRoles []string // *discordgo.GuildRole.Role.ID
+// }
+//
+// // NewCommandPermission creates a new default filled command permission struct
+// func NewCommandPermission() CommandPermission {
+// 	cmdP := CommandPermission{}
+//
+// 	cmdP.BannedUserIDs = []string{}
+// 	cmdP.AllowedRoles = []string{"*"}
+// 	cmdP.AllowedUserIDs = []string{"*"}
+//
+// 	return cmdP
+// }
